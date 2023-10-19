5b9a3c0a81dfac2aba8e1273e65e453e6a253d1a
Jumeaux07 <zouzouacedric@gmail.com>
2023-10-19 17:55:23 +0000

    Template init

--- resources/views/livewire/contact.blade.php
+++ resources/views/livewire/contact.blade.php
@@ -3,32 +3,32 @@
         <div class="row">
             <div class="col-lg-5">
                 <div class="sec-head md-mb80 wow fadeIn">
-                    <h6 class="sub-title mb-15 opacity-7">Get In Touch</h6>
-                    <h2 class="fz-50">Let's make your brand brilliant!</h2>
-                    <p class="fz-15 mt-10">If you would like to work with us or just want to get in
-                        touch, we’d love to hear from you!</p>
+                    {{-- <h6 class="sub-title mb-15 opacity-7">Get In Touch</h6> --}}
+                    <h2 class="fz-50">Rendons votre marque brillante !</h2>
+                    <p class="fz-15 mt-10">Si vous souhaitez travailler avec nous ou si vous souhaitez simplement participer
+                        touchez, nous aimerions avoir de vos nouvelles !</p>
                     <div class="phone fz-30 fw-600 mt-30 underline">
-                        <a href="#0" class="main-color">+1 840 841 25 69</a>
+                        <a href="tel:+2250103772742" class="main-color">+2250103772742</a>
                     </div>
                     <ul class="rest social-text d-flex mt-60">
                         <li class="mr-30">
-                            <a href="#0" class="hover-this"><span class="hover-anim">Facebook</span></a>
+                            <a href="https://www.facebook.com/profile.php?id=100087359930379" class="hover-this"><span class="hover-anim">Facebook</span></a>
                         </li>
                         <li class="mr-30">
-                            <a href="#0" class="hover-this"><span class="hover-anim">Twitter</span></a>
+                            <a href="https://www.instagram.com/essiscedric/" class="hover-this"><span class="hover-anim">Instagram</span></a>
                         </li>
                         <li class="mr-30">
-                            <a href="#0" class="hover-this"><span class="hover-anim">LinkedIn</span></a>
+                            <a href="https://www.linkedin.com/in/essis-cedric-zouzoua-603058272/" class="hover-this"><span class="hover-anim">LinkedIn</span></a>
                         </li>
                         <li>
-                            <a href="#0" class="hover-this"><span class="hover-anim">Instagram</span></a>
+                            <a href="https://github.com/Jumeaux07/" class="hover-this"><span class="hover-anim">Github</span></a>
                         </li>
                     </ul>
                 </div>
             </div>
             <div class="col-lg-7 valign">
                 <div class="full-width wow fadeIn">
-                    <form id="contact-form" method="post" action="https://ui-themez.smartinnovates.net/items/Gavi/light/contact.php">
+                    <form id="contact-form" method="post" action="#">
 
                         <div class="messages"></div>
 
@@ -36,7 +36,7 @@
 
                             <div class="col-lg-6">
                                 <div class="form-group mb-30">
-                                    <input id="form_name" type="text" name="name" placeholder="Name"
+                                    <input id="form_name" type="text" name="name" placeholder="Nom complet"
                                         required="required">
                                 </div>
                             </div>
@@ -61,7 +61,7 @@
                                 </div>
                                 <div class="mt-30">
                                     <button type="submit">
-                                        <span class="text">Send A Message</span>
+                                        <span class="text">Envoyer</span>
                                     </button>
                                 </div>
                             </div>
